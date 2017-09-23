4a4a53c52f0a730568dfe0a22e42e605e7eeadc4
Tiffany <ttawfic@ufl.edu>
2017-09-22 21:33:33 -0400

    s

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -3,23 +3,23 @@ angular.module('listings').factory('Listings', ['$http',
     var methods = {
       
       getAll: function() {
-        return $http.get('http://serene-everglades-86457.herokuapp.com/api/listings');
+        return $http.get('/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://serene-everglades-86457.herokuapp.com/api/listings', listing);
+        return $http.post('/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://http://serene-everglades-86457.herokuapp.com/api/listings' + id);
+        return $http.get('/api/listings' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://serene-everglades-86457.herokuapp.com/api/listings' + id, listing);
+        return $http.put('/api/listings' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://serene-everglades-86457.herokuapp.com/api/listings' + id);
+        return $http.delete('/api/listings' + id);
       }
     };
 
